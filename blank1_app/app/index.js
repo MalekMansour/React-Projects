bf8f532124a1df9be9f3a75d19dbc27803f19aad
Malek Mansour <malek24mansour@gmail.com>
2024-09-12 16:15:32 -0600

    index.js commit

---
+++ blank1_app/app/index.js
@@ -0,0 +1,12 @@
+// class component or functional components
+
+import { Component } from "react";
+
+class Index extends Component {
+    render() {
+        return (
+         
+        );
+    }   
+}
+export default Index;
