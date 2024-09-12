c45b852fba85fa49900099dea1888a331b8b59dc
Malek Mansour <malek24mansour@gmail.com>
2024-09-12 16:41:31 -0600

    App.js Updated

--- blank1_app/App.js
+++ blank1_app/App.js
@@ -1,24 +1,11 @@
-// class component or functional components
+import Index from './app/index.js'; 
 
-import { Component } from "react";
-import { View, Text } from "react-native";
-
-class Index extends Component {
-    render() {
-        return (
-            <View style={{flexDirection: "row", backgroundColor: "red", flex:1}}>
-                <Text style={{fontSize: 40 }}>Hello, World!</Text>
-              <View style={{backgroundColor: "green", flex: 2}}>
-                <Text style={{fontSize: 40 }}>Box1</Text>
-              </View>
-              <View style={{backgroundColor: "grey", flex: 3}}>
-                <Text style={{fontSize: 40 }}>Box2</Text>
-              </View>
-            </View>
-            
-            
-        );
-
-    }   
+function App() {
+  return (
+    <div>
+      <Index />
+    </div>
+  );
 }
-export default Index;
+
+export default App;
