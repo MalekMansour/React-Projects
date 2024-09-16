a7b2b1656469d3627b10f70047f5242738bc45bc
Malek Mansour <malek24mansour@gmail.com>
2024-09-16 12:07:28 -0600

    Update customimage.js

--- blank1_app/components/customimage.js
+++ blank1_app/components/customimage.js
@@ -1,1 +1,17 @@
 //image is corecomponent 
+// local drive and url of an internet image
+
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Customimage = () => {
+  return (
+    <View>
+      <Image></Image>
+    </View>
+  );
+};
+
+export default Customimage;
+
+const styles = StyleSheet.create({})
