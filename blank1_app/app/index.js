3a27fb50c7de1e71bec4881a39464bb9f87bd976
Malek Mansour <malek24mansour@gmail.com>
2024-09-16 14:26:52 -0600

    Update index.js

--- blank1_app/app/index.js
+++ blank1_app/app/index.js
@@ -1,62 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import Custombutton  from '../components/custombutton'  
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Customimage from "../components/customimage";
 
 const Index = () => {
-    const name = "Banana"
   return (
-    <View style={{flex:1, justifyContent:"center", alignItems: "center"}}>
-      <Text style={{fontSize: 25}}>I am in React native class {name} </Text>
-      <Fruit/>
-      <Custombutton/>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 40 }}>Image</Text>
+      <Customimage />
     </View>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
 
-const Fruit=()=>{
-    return(
-        <View>
-        <Text style={{
-            fontSize: 25, 
-            borderColor: "gray", 
-            borderWidth:2, 
-            height:40,
-            backgroundColor: "blue",
-            color: "white"}}>Fruit name is Apple</Text>
-            <Empdata/>
-            </View>
-    )
-}
-
-const Empdata=()=>{
-    const name="John";
-    const id="EMP001";
-    const email="john@gmail.com"
-    return (
-        <View>
-            <Text style={{
-            fontSize: 15, 
-            borderColor: "gray", 
-            borderWidth:2, 
-            height:25,
-            backgroundColor: "coral",}}
-            >Emp Name: {name}</Text>
-            <Text style={{
-            fontSize: 15, 
-            borderColor: "gray", 
-            borderWidth:2, 
-            height:25,
-            backgroundColor: "coral",}}
-            >Emp Id: {id}</Text>
-            <Text style={{
-            fontSize: 15, 
-            borderColor: "gray", 
-            borderWidth:2, 
-            height:25,
-            backgroundColor: "coral",}}
-            >Emp Email: {email}</Text>
-        </View>
-    )
-}
+const styles = StyleSheet.create({});
