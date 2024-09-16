ca03d2a42cf8b909fe9d6434efa01e86621ebe57
Malek Mansour <malek24mansour@gmail.com>
2024-09-16 11:37:06 -0600

    Update index.js

--- blank1_app/app/index.js
+++ blank1_app/app/index.js
@@ -5,7 +5,9 @@ const Index = () => {
     const name = "Banana"
   return (
     <View style={{flex:1, justifyContent:"center", alignItems: "center"}}>
+      <Fruit/>  
       <Text style={{fontSize: 25}}>I am in React native class {name} </Text>
+      <Fruit/>
       <Fruit/>
     </View>
   )
@@ -15,6 +17,12 @@ export default Index
 
 const  Fruit=()=>{
     return(
-        <Text style={{fontSize: 25}}>Fruit name is Apple</Text>
+        <Text style={{
+            fontSize: 25, 
+            borderColor: "gray", 
+            borderWidth:2, 
+            height:40,
+            backgroundColor: "blue",
+            color: "white"}}>Fruit name is Apple</Text>
     )
 }
