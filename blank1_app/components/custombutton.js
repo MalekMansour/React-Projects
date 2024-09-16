603962544482493194deb106b1215e4783e41ec6
Malek Mansour <malek24mansour@gmail.com>
2024-09-16 11:57:21 -0600

    Update custombutton.js

---
+++ blank1_app/components/custombutton.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const custombutton = () => {
+  return (
+    <View>
+      <Text>custombutton</Text>
+    </View>
+  )
+}
+
+export default custombutton
+
+const styles = StyleSheet.create({})
