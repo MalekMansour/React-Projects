066aba58ed75d67507085dfb79152c0dc355676e
Malek Mansour <malek24mansour@gmail.com>
2024-09-16 11:34:36 -0600

    Update index.js

--- blank1_app/app/index.js
+++ blank1_app/app/index.js
@@ -2,9 +2,10 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 const Index = () => {
+    const name = "Banana"
   return (
     <View style={{flex:1, justifyContent:"center", alignItems: "center"}}>
-      <Text style={{fontSize: 25}}>I am in React native class</Text>
+      <Text style={{fontSize: 25}}>I am in React native class {name} </Text>
       <Fruit/>
     </View>
   )
