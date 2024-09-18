83c7abd30d550ee98c49f6c4c568c8f2db65f5e8
Malek Mansour <malek24mansour@gmail.com>
2024-09-18 01:33:59 -0600

    Update fclasses.js

--- Lab0/app/jsreview/fclasses.js
+++ Lab0/app/jsreview/fclasses.js
@@ -1,15 +1,30 @@
-// ...
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    getArea() {
+        return this.width * this.height;
+    }
+}
+
+class Square extends Rectangle {
+    constructor(side) {
+        super(side, side);
+    }
+
+    getPerimeter() {
+        return 4 * this.width;
+    }
+}
 
 const rect = new Rectangle(10, 20);
-
-console.log('Rectangle area: ' + rect.getArea());
-
-// ...
+console.log('Rectangle area: ' + rect.getArea());  
 
 const square = new Square(25);
-
-console.log('Square perimeter: ' + square.getPerimeter());
-console.log('Square area: ' + square.getArea());
+console.log('Square perimeter: ' + square.getPerimeter());  
+console.log('Square area: ' + square.getArea());            
 
 /*
  * Expected output:
