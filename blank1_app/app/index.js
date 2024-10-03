f1d7d2828e96d4f496d8cc8d897b8f979beb6557
Malek Mansour <malek24mansour@gmail.com>
2024-10-03 16:31:51 -0600

    Commit

--- blank1_app/app/index.js
+++ blank1_app/app/index.js
@@ -1,15 +1,36 @@
-import { StyleSheet, View, Image} from "react-native"; 
-import React from "react";
-import TextInputCustom from "../components/textinputcustom"; 
+import { StyleSheet, Text, View } from "react-native";
+import Practisecounter from "../components/practisecounter";
+import Practisetxtin from "../components/practisetxtin";
+import InstaLikedPost from "../components/instalikedpost";
 
-const Index = () => {
+export default function Page() {
   return (
-    <View style={{ flex: 1 }}>
-      <TextInputCustom />
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <InstaLikedPost />
+      </View>
     </View>
   );
-};
+}
 
-export default Index;
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 36,
+    color: "#38434D",
+  },
+});
